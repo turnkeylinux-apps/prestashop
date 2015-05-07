da0689d1e3f88ea0651cb16c6e3ca96e2e187d61
Qrntz <q@dae.pp.ua>
2015-05-07 18:25:43 +0000

    Change minimum password length to 8 according to PrestaShop default

--- overlay/usr/lib/inithooks/bin/prestashop.py
+++ overlay/usr/lib/inithooks/bin/prestashop.py
@@ -59,7 +59,7 @@ def main():
         password = d.get_password(
             "PrestaShop Password",
             "Enter new password for the PrestaShop '%s' account." % email,
-            min_length=5)
+            min_length=8)
 
     if not domain:
         if 'd' not in locals():
